6b4491db42a3765421b081ab7535dc2a8d63e6d4
Patrick Leary <paleary@davidson.edu>
2023-09-13 00:33:31 -0400

    Added a Figma link and JS stuff

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,3 +18,14 @@ Staaatliches Bauhaus (1919-1933)
 `;
 
 console.log(bauhaus);
+
+/* I wasn't sure how to do the JS stuff, so I looked it up -PL */
+console.log(document.querySelectorAll("div"));
+
+const mousePosText = document.getElementById('mouse-pos');
+let mousePos = { x: undefined, y: undefined };
+
+window.addEventListener('mousemove', (event) => {
+  mousePos = { x: event.clientX, y: event.clientY };
+  mousePosText.textContent = `(${mousePos.x}, ${mousePos.y})`;
+});
